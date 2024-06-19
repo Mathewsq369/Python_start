fc172292ee30664bd185a01df2ffe14a41732c6c
Mathewsq369 <mathewsq369@gmail.com>
2024-06-20 01:12:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+from math import sqrt
+
+
+def root(number):
+    return sqrt(number)
+
+
+print(root(7))
+
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print("Make: " + self.make + " Model: " + self.model + " Year: " + str(self.year))
+
+car1 = Car("Toyota","Corolla",2022)
+
+car1.display_info()
+
+
+class Shape:
+    def area(self):
+        return 0
+
+class Rectangle(Shape):
+    def area(self,length,width):
+        return length * width
+
+
+class Circle(Shape):
+    def area(self,radius):
+        return 3.14 * radius ** 2
+
+rectangle = Rectangle()
+result_rectangle = rectangle.area(4,5)
+print(result_rectangle)
+
+circle = Circle()
+result_circle = circle.area(3)
+print(result_circle)
+
+
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+    result = num1 / num2
+    print("Result:",result)
+except ZeroDivisionError:
+    print("Error: cannot divide by zero.")
+except ValueError:
+    print("Error: Invalid input.")
+
+
+
+file = open("myfile.txt", "w")
+file.write("Programming is fun")
+file.close()
+
+file = open("myfile.txt", "r")
+content = file.read()
+print(content)
+file.close()
+
+
+
+
+class ToDoListManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self,task):
+        self.tasks.append(task)
+
+    def view_tasks(self):
+        for task in self.tasks:
+            print(task)
+
+    def delete_task(self,task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+            print("task deleted")
+        else:
+            print("task not found")
+
+
+
+manager = ToDoListManager()
+manager.add_task("Wash Dishes")
+manager.add_task("Cook Ugali")
+manager.add_task("Take a shower")
+manager.view_tasks()
+manager.delete_task("Washroom")
